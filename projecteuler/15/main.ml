4f8f8d5d20eda7c0b203c8a4c718c5f1bfa05567
tony chang <tony@ponderer.org>
2008-08-25 16:32:55 -0700

    solution to problem 15

---
+++ projecteuler/15/main.ml
@@ -0,0 +1,22 @@
+open Printf;;
+open Hashtbl;;
+
+let mem = Hashtbl.create (21 * 21);;
+
+let rec countRoutes rows cols =
+  if rows = 0 or cols = 0 then
+    1
+  else begin
+    if Hashtbl.mem mem (rows, cols) then
+      Hashtbl.find mem (rows, cols)
+    else begin
+      let routes = countRoutes (rows - 1) cols +
+                   countRoutes rows (cols - 1)
+      in
+        Hashtbl.add mem (rows, cols) routes;
+        routes
+    end
+  end
+;;
+
+Printf.printf "%d\n" (countRoutes 20 20);;
