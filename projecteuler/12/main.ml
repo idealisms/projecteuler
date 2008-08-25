edc60e77abba416aaf80c42cbbed0af52a6ee9b2
tony chang <tony@ponderer.org>
2008-08-25 12:56:12 -0700

    solution to 12

--- projecteuler/12/main.ml
+++ projecteuler/12/main.ml
@@ -1,1 +1,25 @@
-open Printf;
+open Printf;;
+
+let rec countFactors n div factors =
+  if div * div = n then
+    factors + 1
+  else if div * div > n then
+    factors
+  else begin
+    if n mod div = 0 then
+      countFactors n (div + 1) (factors + 2)
+    else
+      countFactors n (div + 1) factors
+  end
+;;
+
+let rec findFiveHundred n step =
+  let factors = (countFactors n 1 0)
+  in
+    if factors > 500 then
+      n
+    else
+      findFiveHundred (n + step) (step + 1)
+;;
+
+Printf.printf "%d\n" (findFiveHundred 1 2);;
